a9791a6f93f688db899de50a59dfd31731a816f1
Tucker Mogren <mogrent@hotmail.com>
2023-03-06 20:44:52 -0500

    Moving old working code into dev

---
+++ src/Example/Automapper/Domain/Interfaces/IEmployeeDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Domain.Interfaces;
+
+	public interface IEmployeeDTO
+	{
+
+    public string? FullName { get; set; }
+    public int? Salary { get; set; }
+    public string? Address { get; set; }
+    public string? Dept { get; set; }
+}
+
---
+++ src/Example/Automapper/EmployeeAPI/Configurations/AutomapperConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AutoMapper;
+using Data.Models;
+using Domain.DTOModels;
+
+namespace EmployeeAPI.Configurations
+{ 
+
+	[ExcludeFromCodeCoverage]
+	public static class AutomapperConfiguration
+	{
+
+		public static void ConfigureAutoMapper (this IServiceCollection services)
+		{
+			var mappingConfig = MappingConfig();
+
+			IMapper mapper = mappingConfig.CreateMapper();
+
+			services.AddSingleton(mapper);
+		}
+
+
+		public static MapperConfiguration MappingConfig()
+		{
+			return new MapperConfiguration(mc =>
+			{
+				mc.CreateMap<Employee, EmployeeDto>()
+					.ForMember(dto => dto.Dept, act => act
+						.MapFrom(src => src.Department))
+					.ForMember(dto => dto.FullName, act => act
+						.MapFrom(mapExpression: src => string.Concat(src.FirstName, " ", src.LastName)))
+					.ReverseMap();
+
+				mc.CreateMap<EmployeeDto, Employee>()
+					.ForMember(emp => emp.Id, act => act.Ignore())
+					.ForMember(emp => emp.FirstName, act => act.MapFrom(x => x.FullName.Split()[0]))
+                    .ForMember(emp => emp.LastName, act => act.MapFrom(x => x.FullName.Split()[1]))
+                    .ForMember(emp => emp.Department, act => act.MapFrom(src => src.Dept))
+                        .ReverseMap();
+
+			});
+		}
+	}
+}
+
+
