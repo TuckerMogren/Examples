1b3effb19e15a541e7c538e12b9adce1867d2b4c
Tucker Mogren <mogrent@hotmail.com>
2023-03-06 20:42:27 -0500

    moving over old code

---
+++ src/Example/Automapper/Example.Automapper.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Automapper.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : ControllerBase
+{
+
+    private readonly ILogger<EmployeeController> _logger;
+
+    public EmployeeController(ILogger<EmployeeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("AddEmployee")]
+    public async Task<IActionResult> AddEmployee()
+    {
+
+        return Ok();
+    }
+
+}
+
