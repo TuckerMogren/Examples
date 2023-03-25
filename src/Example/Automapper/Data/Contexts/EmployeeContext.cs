ee6df2bc120d4c86706cc68afab449b3972be42e
Tucker Mogren <mogrent@hotmail.com>
2023-03-25 12:29:41 -0400

    fixing warnings and testing to see if build pipeline failes

--- src/Example/Automapper/Data/Contexts/EmployeeContext.cs
+++ src/Example/Automapper/Data/Contexts/EmployeeContext.cs
@@ -1,23 +1,29 @@
-﻿using System;
+﻿using System.Data.Entity.ModelConfiguration.Conventions;
 using Data.Models;
-using System.Data;
-using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Data.Contexts
 {
 	public class EmployeeContext : DbContext
-	{
-		public EmployeeContext()
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public EmployeeContext()
+        {
+
+        }
+
+		protected override void OnModelCreating(ModelBuilder model)
 		{
-
-		}
-
-		public DbSet<Employee> Employees { get; set; }
-
-		protected override void OnModelCreating(DbModelBuilder modelBuilder)
-		{
-			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(model);
+            model.Entity<Employee>(e =>
+            {
+                e.ToTable("Emoloyees");
+                e.HasKey(e => e.Id);
+                e.Property(e => e.Id).IsRequired();
+            });
         }
 
 	}
