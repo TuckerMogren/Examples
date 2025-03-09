622cd037862fda1bfe46cb2a69c0cbf5a3a5ee5f
Tucker Mogren <mogrent@hotmail.com>
2025-03-09 19:20:56 -0400

    adding redoc in non prod envs

--- src/Example/Inheritance/InheritancePlayground.Services/Program.cs
+++ src/Example/Inheritance/InheritancePlayground.Services/Program.cs
@@ -2,14 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
-    app.MapOpenApi();
+    app.UseReDoc(c =>
+    {
+        c.RoutePrefix = "docs"; // Access API docs at `/docs`
+        c.DocumentTitle = "API Documentation";
+    });
 }
 
 app.UseHttpsRedirection();
