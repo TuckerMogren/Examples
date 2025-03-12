9ab97a8f8b73ffc7526953ea60b1bc335363abb7
Tucker Mogren <mogrent@hotmail.com>
2025-03-11 20:17:18 -0400

    adding test

--- src/Example/PerformanceTesting/PerformanceTestingApp/GuidPerformanceTest.cs
+++ src/Example/PerformanceTesting/PerformanceTestingApp/GuidPerformanceTest.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 public class GuidPerformanceTest
 {
     private Guid _guid;
+    private static List<Guid> _guidList = new();
+
+    private string _string;
+    private static List<string> _stringList = new();
 
     [GlobalSetup]
     public void Setup()
@@ -13,15 +19,19 @@ public class GuidPerformanceTest
     }
 
     [Benchmark]
+    [MethodImpl(MethodImplOptions.NoInlining)]
     public string GuidToString()
     {
-        return _guid.ToString();
+        var str = _guid.ToString();
+        _stringList.Add(str);
+        return str;
     }
 
     [Benchmark]
+    [MethodImpl(MethodImplOptions.NoInlining)]
     public Guid UseGuidDirectly()
     {
+        _guidList.Add(_guid);
         return _guid;
     }
 }
-
